366131d85bb95e83f83190158185a54c7cfcd894
Alwin1847207 <alwin.joseph@mca.christuniversity.in>
2020-09-26 19:02:21 +0530

    changes

--- routes/user.js
+++ routes/user.js
@@ -1,34 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var productHelper = require('../helpers/product-helpers')
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-
-    let products = [
-        {
-            name: "I PHONE 11",
-            category: 'Mobile',
-            description: 'This is a good phone',
-            image:'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-11-pro-select-2019-family?wid=882&amp;hei=1058&amp;fmt=jpeg&amp;qlt=80&amp;op_usm=0.5,0.5&amp;.v=1586586488946'
-            }, {
-            name: "I PHONE 12",
-            category: 'Mobile',
-            description: 'This is a good phone',
-            image:'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-11-pro-select-2019-family?wid=882&amp;hei=1058&amp;fmt=jpeg&amp;qlt=80&amp;op_usm=0.5,0.5&amp;.v=1586586488946'
-        }, {
-            name: "I PHONE 10",
-            category: 'Mobile',
-            description: 'This is a good phone',
-            image:'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-11-pro-select-2019-family?wid=882&amp;hei=1058&amp;fmt=jpeg&amp;qlt=80&amp;op_usm=0.5,0.5&amp;.v=1586586488946'
-        }, {
-            name: "I PHONE 13",
-            category: 'Mobile',
-            description: 'This is a good phone',
-            image:'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-11-pro-select-2019-family?wid=882&amp;hei=1058&amp;fmt=jpeg&amp;qlt=80&amp;op_usm=0.5,0.5&amp;.v=1586586488946'
-        },
-    ]
-
-    res.render('index', {products,admin:false});
+    productHelper.getAllProducts().then((products)=>{
+        res.render('user/view-products',{admin:true,products})
+    })
 });
 
 module.exports = router;
